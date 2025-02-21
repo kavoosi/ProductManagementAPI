5722317f98b170e9fd8e43f59cd96c2bcf19730c
Amir Kavoosi <amir4x2005@yahoo.com>
2025-02-21 09:06:22 -0500

    Added ProductContext for database context

--- ProductManagementAPI/Model/Product.cs
+++ ProductManagementAPI/Model/Product.cs
@@ -1,6 +1,28 @@
-﻿namespace ProductManagementAPI.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementAPI.Model
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public required string Title { get; set; }
+
+        [MaxLength(5000)]
+        public string? Description { get; set; }
+
+        [Required]
+        [RegularExpression("^(S|M|L|XL)$", ErrorMessage = "Allowed values: S, M, L, XL")]
+        public required string EstimatedComplexity { get; set; }
+
+        [Required]
+        [RegularExpression("^(New|Active|Closed|Abandoned)$", ErrorMessage = "Allowed values: New, Active, Closed, Abandoned")]
+        public required string Status { get; set; }
+
+        public DateTime? TargetCompletionDate { get; set; }
+        public DateTime? ActualCompletionDate { get; set; }
     }
 }
---
+++ ProductManagementAPI/Model/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ProductManagementAPI.Model
+{
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // در صورت نیاز تنظیمات خاص مدل‌ها را اینجا انجام بده
+        }
+    }
+}
