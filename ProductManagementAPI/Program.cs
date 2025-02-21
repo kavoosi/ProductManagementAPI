e7e36921c078c72d5d0d23c9478b20d1f2f3759a
Amir Kavoosi <amir4x2005@yahoo.com>
2025-02-21 08:47:39 -0500

    Initial commit - Added .NET Core API project

---
+++ ProductManagementAPI/Program.cs
@@ -0,0 +1,36 @@
+namespace ProductManagementAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
